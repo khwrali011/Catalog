88d4f359d243ee6def5f98c9099ebb528f7c0685
khwrali011 <khawar125ali@gmail.com>
2025-02-26 01:28:32 +0500

    credentials moved to json and main query changed

--- connect.py
+++ connect.py
@@ -1,11 +1,22 @@
 import mysql.connector
+from pathlib import Path
+import os
+import json
+
+App_Directory = Path(__file__).parent
+
+credentials_path = os.path.join(App_Directory, "credenials.json")
+
+# Open and read the JSON file
+with open(credentials_path, "r") as file:
+    data = json.load(file)  # Parse JSON
 
 # MySQL Database Configuration
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'root',
-    'database': 'rectureai'
+    'host': data['host'],
+    'user': data['user'],
+    'password': data['password'],
+    'database': data['database']
 }
 
 def get_db_connection():
--- sql_queries.py
+++ sql_queries.py
@@ -3,9 +3,19 @@ from connect import get_db_connection
 from cryptography.fernet import Fernet
 import base64
 import hashlib
+from pathlib import Path
+import os
+
+App_Directory = Path(__file__).parent
+
+credentials_path = os.path.join(App_Directory, "credenials.json")
+
+# Open and read the JSON file
+with open(credentials_path, "r") as file:
+    data = json.load(file)  # Parse JSON
 
 # Custom key 
-custom_key = "03334337976"
+custom_key = data['encryptionkey']
 
 # Derive a 32-byte key using SHA256 and then base64 encode it
 hashed_key = hashlib.sha256(custom_key.encode()).digest()
@@ -43,7 +53,7 @@ def return_client_data(clientId):
         c.responseAPI,
         c.lectureRouteUrl,
         c.lectureRouteUrlServer,
-        c.clientSecertKey,
+        c.lectureDetailUrl,
         c.isActive AS clientIsActive,
         c.createdOn AS clientCreatedOn,
         r.relationId,
@@ -60,7 +70,7 @@ def return_client_data(clientId):
     FROM rectureai.tbl_client c
     LEFT JOIN rectureai.tbl_relation_client_package r ON c.clientId = r.clientId
     LEFT JOIN rectureai.tbl_packages p ON r.packageId = p.packageId
-    WHERE c.clientId = %s AND c.isActive = 1 AND r.remaining_lecture > 0;
+    WHERE c.clientId = %s;
     """
 
     conn = get_db_connection()
